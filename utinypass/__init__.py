a8c85cb2cfb93abd79a86182319085d5f986290f
Glenn Franxman <glenn.franxman@scripps.com>
2015-02-09 11:54:05 -0500

    Adding a docstring to the top-level package.

--- utinypass/__init__.py
+++ utinypass/__init__.py
@@ -1,3 +1,13 @@
+''' An UNOFFICIAL TinyPass untility library.
+
+Use like:
+
+    import utinypass.crypto
+
+    encrypted = utinypass.crypt.b64encode( 'mykey', 'mydata' ) 
+
+'''
+
 __all__ = [ 'crypto', ]
 __version__ = '0.1.0'
 
